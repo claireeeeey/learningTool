5ae41b6060fa3abfccbbb92db9dbb8e79c8a460c
claireeeeey <90796587+claireeeeey@users.noreply.github.com>
2022-07-26 21:24:56 -0500

    Update firebase-connector.js

--- firebase-connector.js
+++ firebase-connector.js
@@ -15,22 +15,5 @@ let myFlashcards = [ {
     }
   ];
 
-function initializeFirebase () {
-  const firebaseConfig = {
-    apiKey: "AIzaSyCucqrbubrsMi31OmxroTUIJsUsm5x3-ew",
-    authDomain: "best-learning-tool-b5156.firebaseapp.com",
-    databaseURL: "https://best-learning-tool-b5156-default-rtdb.firebaseio.com",
-    projectId: "best-learning-tool-b5156",
-    storageBucket: "best-learning-tool-b5156.appspot.com",
-    messagingSenderId: "101366930945",
-    appId: "1:101366930945:web:8f2e6f633845e737f2aa00",
-    measurementId: "G-3PR2PQRBPV"
-  };
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-}
-
-initializeFirebase();
-
 let dbLocation = firebase.database().ref('deck/flashcards'); 
 dbLocation.set(myFlashcards);
