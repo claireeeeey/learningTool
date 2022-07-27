154a34e3288111e71d23895a414c611c6d227b81
claireeeeey <90796587+claireeeeey@users.noreply.github.com>
2022-07-27 18:56:31 -0500

    Update firebase-connector.js

--- firebase-connector.js
+++ firebase-connector.js
@@ -13,8 +13,23 @@ let myFlashcards = [ {
     "image" : "images/hummingbird-2139279__340.webp",
     "answer": "Hummingbird"
     }
-  ];
-  
+  ];  
+
+function initializeFirebase () {
+  var firebaseConfig = {
+    apiKey: "AIzaSyCucqrbubrsMi31OmxroTUIJsUsm5x3-ew",
+    authDomain: "best-learning-tool-b5156.firebaseapp.com",
+    databaseURL: "https://best-learning-tool-b5156-default-rtdb.firebaseio.com",
+    projectId: "best-learning-tool-b5156",
+    storageBucket: "best-learning-tool-b5156.appspot.com",
+    messagingSenderId: "101366930945",
+    appId: "1:101366930945:web:8f2e6f633845e737f2aa00",
+    measurementId: "G-3PR2PQRBPV"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
+
 function writeStudentAnswers(name, email, answers) {
   firebase.database().ref('users/' + name).set({
     username: name,
@@ -24,4 +39,6 @@ function writeStudentAnswers(name, email, answers) {
   let dbLocation = firebase.database().ref('deck/flashy')
   dbLocation.set(myFlashcards);
 }
+
+initializeFirebase();
 writeStudentAnswers("michael","foo@bar.com", ["b","c","e","f"]);
