1ed77cb4abeaa1d84f6d74acbe7539fd9d1e94cc
claireeeeey <90796587+claireeeeey@users.noreply.github.com>
2022-07-26 20:29:34 -0500

    Update firebase-connector.js

--- firebase-connector.js
+++ firebase-connector.js
@@ -35,44 +35,12 @@ let myFlashcards = [ {
      ];
 
 
-function initializeFirebase() {
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCucqrbubrsMi31OmxroTUIJsUsm5x3-ew",
-    authDomain: "best-learning-tool-b5156.firebaseapp.com",
-    projectId: "best-learning-tool-b5156",
-    storageBucket: "best-learning-tool-b5156.appspot.com",
-    messagingSenderId: "101366930945",
-    appId: "1:101366930945:web:8f2e6f633845e737f2aa00",
-    measurementId: "G-3PR2PQRBPV"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-}
-
-function writeStudentAnswers(name, email, answers) {
-  firebase.database().ref('users/' + name).set({
-    username: name,
-    email: email,
-    answers : answers
-  });
-  let dbLocation = firebase.database().ref('deck/flashy')
-  dbLocation.set(myFlashcards);
-}
-
-initializeFirebase();
-writeStudentAnswers("michael","foo@bar.com", ["b","c","e","f"]);
-writeStudentAnswers("jennifer","third@floor.com", ["45","Gettysburg address","Windows 11","french fries"]);
-
-let fc = firebase.database().ref('deck/flashy');
-console.log('Output: ' + fc.toString() );
-console.log('JSON: ' + fc.toJSON() );
-
-let qd;
-fc.on("value", function(retrieve) {
-    let queryData = retrieve.val();
-    console.log('Real JSON:' + JSON.stringify(queryData) );
-    qd = JSON.stringify(queryData);
-});
-console.log('Real Real JSON:' + qd );
+// Make the database point to the location root -> deck -> flashcards
+// If the location doesn't exist is will be created
+// Create the reference location
+let dbLocation = firebase.database().ref('deck/flashcards'); 
+ 
+// myFlashcards will be stored under flashcards in the database
+// Anything that was in this location will be overwritten
+// Thus, a write operation also does an update
+dbLocation.set(myFlashcards);
