6ba5bb6b0aeebcdaf4b9b1173a8f6d4b804a3d32
claireeeeey <90796587+claireeeeey@users.noreply.github.com>
2022-07-26 19:53:26 -0500

    Update firebase-connector.js

--- firebase-connector.js
+++ firebase-connector.js
@@ -1,24 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+function initializeFirebase () {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCucqrbubrsMi31OmxroTUIJsUsm5x3-ew",
+    authDomain: "best-learning-tool-b5156.firebaseapp.com",
+    projectId: "best-learning-tool-b5156",
+    storageBucket: "best-learning-tool-b5156.appspot.com",
+    messagingSenderId: "101366930945",
+    appId: "1:101366930945:web:8f2e6f633845e737f2aa00",
+    measurementId: "G-3PR2PQRBPV"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCucqrbubrsMi31OmxroTUIJsUsm5x3-ew",
-  authDomain: "best-learning-tool-b5156.firebaseapp.com",
-  projectId: "best-learning-tool-b5156",
-  storageBucket: "best-learning-tool-b5156.appspot.com",
-  messagingSenderId: "101366930945",
-  appId: "1:101366930945:web:8f2e6f633845e737f2aa00",
-  measurementId: "G-3PR2PQRBPV"
-};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 let myFlashcards = [ {
     "question": "Name this species of bird.",
