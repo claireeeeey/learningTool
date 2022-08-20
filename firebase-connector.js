fd5f659ee6971ce88258529d40a7307f751f271f
claireeeeey <90796587+claireeeeey@users.noreply.github.com>
2022-08-20 03:55:07 -0500

    Update firebase-connector.js

--- firebase-connector.js
+++ firebase-connector.js
@@ -2,18 +2,22 @@ import {getDatabase, ref, set, update, child, get,
 onValue, remove} from "https://www.gstatic.com/firebasejs/9.9.3/firebasedatabase.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
-const firebaseConfig = {
-  apiKey: "AIzaSyB7PyoBopqhHlDIeMtIfZc9tP2PyWdPEOQ",
-  authDomain: "best-learning-tool-9281c.firebaseapp.com",
-  databaseURL: "https://best-learning-tool-9281c-default-rtdb.firebaseio.com",
-  projectId: "best-learning-tool-9281c",
-  storageBucket: "best-learning-tool-9281c.appspot.com",
-  messagingSenderId: "988183622580",
-  appId: "1:988183622580:web:9677979a51d67f58d0f8e1",
-  measurementId: "G-B6YY6VSY25"
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+function initializeFirebase() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyB7PyoBopqhHlDIeMtIfZc9tP2PyWdPEOQ",
+    authDomain: "best-learning-tool-9281c.firebaseapp.com",
+    databaseURL: "https://best-learning-tool-9281c-default-rtdb.firebaseio.com",
+    projectId: "best-learning-tool-9281c",
+    storageBucket: "best-learning-tool-9281c.appspot.com",
+    messagingSenderId: "988183622580",
+    appId: "1:988183622580:web:9677979a51d67f58d0f8e1",
+    measurementId: "G-B6YY6VSY25"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
+
+initializeFirebase();
 
 let myFlashcard = [
   {
