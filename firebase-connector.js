2b894b7b5053794f5bfbe9f45e3078e28ac85f68
claireeeeey <90796587+claireeeeey@users.noreply.github.com>
2022-08-20 03:42:19 -0500

    Update firebase-connector.js

--- firebase-connector.js
+++ firebase-connector.js
@@ -2,9 +2,9 @@
 //
 import { getDatabase, ref, set, update, child, get,
 onValue, remove } from
-"https://www.gstatic.com/firebasejs/9.9.2/firebasedatabase.js (https://www.gstatic.com/firebasejs/9.9.2/firebasedatabase.js)";
+"https://www.gstatic.com/firebasejs/9.9.3/firebasedatabase.js (https://www.gstatic.com/firebasejs/9.9.3/firebasedatabase.js)";
 <!-- define the script as a module. This is a special type of JavaScript file that allows you to include other JavaScript libraries. -->
-<script type="module" src="firebase-connector.js">
+
   //Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
@@ -26,7 +26,6 @@ onValue, remove } from
   //Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-</script>
 
 let myFlashcards = [ {
 "question": "Name this species of bird.",
